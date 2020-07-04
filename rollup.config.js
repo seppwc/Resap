6735f852ec5bf2ad6ee9bcfe5211ee1db28c98a9
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-07-04 17:04:51 +0100

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,24 @@
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
-import pkg from './package.json'
 
 export default {
   input: 'src/index.ts', // our source file
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        gsap: 'gsap',
-      },
+  output: {
+    file: './lib/bundle.min.js',
+    format: 'iife',
+    name: 'bundle',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      gsap: 'gsap',
     },
-    {
-      file: pkg.module,
-      format: 'es', // the preferred format
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        gsap: 'gsap',
-      },
-    },
-    {
-      file: pkg.browser,
-      format: 'iife',
-      name: 'resapBundle', // the global which can be used in a browser
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        gsap: 'gsap',
-      },
-    },
-  ],
+  },
   external: [
     // makes dependancies and peer dependancies available to bundles
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
+    'react',
+    'react-dom',
+    'gsap',
   ],
   plugins: [
     typescript({
@@ -46,10 +26,6 @@ export default {
       typescript: require('typescript'),
     }),
     terser(), // minifies generated bundles
-    resolve({
-      customResolveOptions: {
-        moduleDirectory: 'node_modules',
-      },
-    }),
+    resolve(),
   ],
 }

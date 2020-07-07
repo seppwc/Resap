4bc939bde285b7411759c4aa225a69895a408ae5
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-07-07 09:52:05 +0100

    reconfigure rollup cjs esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,44 @@
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
-import globals from 'rollup-plugin-node-globals'
 import commonjs from '@rollup/plugin-commonjs'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-// eslint-disable-next-line import/no-extraneous-dependencies
-import externalGlobals from 'rollup-plugin-external-globals'
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.ts', // our source file
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      globals: [{ react: 'React', gsap: 'gsap' }],
+const input = 'src/index.ts'
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+]
+
+const plugins = [
+  typescript({
+    // eslint-disable-next-line global-require
+    typescript: require('typescript'),
+  }),
+  terser(), // minifies generated bundles
+  commonjs(),
+  resolve({
+    mainFields: ['module', 'main'],
+    extensions,
+    dedupe: ['react', 'react-dom'],
+  }),
+]
+
+const createConfig = (inpt, outpt, format) => {
+  return {
+    input: inpt,
+    output: {
+      file: outpt,
+      format,
+      sourcemap: true,
     },
-  ],
-  external: [
-    // makes dependancies and peer dependancies available to bundles
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    typescript({
-      // eslint-disable-next-line global-require
-      typescript: require('typescript'),
-    }),
-    terser(), // minifies generated bundles
-    commonjs(),
-    resolve({
-      mainFields: ['module', 'main'],
-      extensions: ['.js', '.ts', '.map'],
-      dedupe: ['react', 'react-dom'],
-    }),
-    globals(),
-    peerDepsExternal(),
-    externalGlobals({
-      react: 'React',
-    }),
-  ],
+    plugins,
+    external,
+  }
 }
+
+export default [
+  createConfig(input, pkg.main, 'cjs'),
+  createConfig(input, pkg.module, 'esm'),
+]

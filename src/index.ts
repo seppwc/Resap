4839b63bf1e787e1a87bce9a42c1fa612fec0e02
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-07-07 17:01:19 +0100

    fixed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import gsap, { CSSPlugin } from 'gsap'
-import React from 'react'
 
-React.useEffect(() => {
-  gsap.registerPlugin(CSSPlugin)
-}, [])
+gsap.registerPlugin(CSSPlugin)
 
 export { useToggle } from './hooks/useToggle'
 export { useGSAP } from './hooks/useGSAP'
